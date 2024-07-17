6291805775eb15f4aa93c72f52b3140175284b54
Komal-Kalsariya <komalkalsariya233@gmail.com>
2024-07-17 11:57:04 +0530

    simpleform

--- src/Form.jsx
+++ src/Form.jsx
@@ -8,16 +8,56 @@ const Form = () => {
         email:"",
         password:""
     })
-
+    let[login,setLogin]=useState({
+        useremail:"",
+        userpassword:"",
+    
+    })
+    let [islogin,setIslogin]=useState(false)
+const handlelogin=(e)=>{
+    e.preventDefault()
+    let {name,value}=e.target
+    setLogin({...login,[name]:value})
+}
  const handelData=(e)=>{
     e.preventDefault()
     let {name,value}=e.target
     console.log(name,value);
     setUserData({...userdata,[name]:value})
  } 
+ const userData=(e)=>{
+    e.preventDefault()
+    if(userdata.email==login.useremail && userdata.password == login.userpassword){
+        toast.success('🦄 Login success!!!!', {
+            position: "top-center",
+            autoClose: 5000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "colored",
+            transition: Bounce,
+            });
+    }
+    else{
+        toast.error('🦄 login fail!', {
+            position: "top-center",
+            autoClose: 5000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "colored",
+            transition: Bounce,
+            });
+    }
+ }
  const handleSubmit=(e)=>{
     e.preventDefault()
     if(userdata.username.length>0 && userdata.email.length>0 && userdata.password.length>0){
+        setIslogin(true)
         toast.success('🦄 suceess!', {
             position: "top-center",
             autoClose: 5000,
@@ -31,6 +71,7 @@ const Form = () => {
             });
     }
     else{
+        setIslogin(false)
         toast.success('🦄 Fail!', {
             position: "top-center",
             autoClose: 5000,
@@ -46,22 +87,36 @@ const Form = () => {
  }  
   return (
     <div>
-        <ToastContainer/>
-        <h2>SignUp Page</h2>
-        <form onSubmit={handleSubmit}>
-            <label>UserName:</label>
-            <input type="text" placeholder='Enter Username'  value={userdata.username} name='username' onChange={handelData}/>
-            <br />
-            <label>Eamil:</label>
-            <input type="email" placeholder='Enter Eamil' value={userdata.email} name='email' onChange={handelData}/>
-            <br />
-            <label>Password:</label>
-            <input type="text" placeholder='Enter Password' value={userdata.password} name='password' onChange={handelData} />
-            <br />
-            <button type='submit'>Sigin</button>
-        </form>
+         <ToastContainer/> 
+        {
+            islogin?(
+                <form onSubmit={userData}>
+                     <label>Eamil:</label>
+                <input type="email" placeholder='Enter Eamil' value={login.email} name='useremail' onChange={handlelogin}/>
+                <br />
+                <label>Password:</label>
+                <input type="text" placeholder='Enter Password' value={login.password} name='userpassword' onChange={handlelogin} />
+                <br />
+                <button type='submit'>login</button>
+                </form>
+            )
+        :(
+            
+            <form onSubmit={handleSubmit}>
+                <label>UserName:</label>
+                <input type="text" placeholder='Enter Username'  value={userdata.username} name='username' onChange={handelData}/>
+                <br />
+                <label>Eamil:</label>
+                <input type="email" placeholder='Enter Eamil' value={userdata.email} name='email' onChange={handelData}/>
+                <br />
+                <label>Password:</label>
+                <input type="text" placeholder='Enter Password' value={userdata.password} name='password' onChange={handelData} />
+                <br />
+                <button type='submit'>Sigin</button>
+            </form>
+        )}
     </div>
   )
-}
 
+}
 export default Form
